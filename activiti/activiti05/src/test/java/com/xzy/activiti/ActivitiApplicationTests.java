48664bb978f6e0583d957a0121ee2138a95d8868
xiaozhengyu <zhengyupython@qq.com>
2020-12-23 21:03:41 +0800

    流程定义、流程部署

--- activiti/activiti05/src/test/java/com/xzy/activiti/ActivitiApplicationTests.java
+++ activiti/activiti05/src/test/java/com/xzy/activiti/ActivitiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.xzy.activiti;
 
 import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 class ActivitiApplicationTests {
 
     /**
-     * 使用activiti默认的方式来创建数据库表
+     * 测试流程部署
      */
     @Test
-    void contextLoads() {
+    void testProcessDeployment() {
 
         // 1.获取ProcessEngineConfiguration
         //ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("配置文件名");
@@ -24,12 +25,21 @@ class ActivitiApplicationTests {
         // 2.获取ProcessEngine
         ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
 
-        // 3.获取相关Service
+        // 3.获取RepositoryService：activiti资源管理类
         RepositoryService repositoryService = processEngine.getRepositoryService();
-        RuntimeService runtimeService = processEngine.getRuntimeService();
-        TaskService taskService = processEngine.getTaskService();
-        HistoryService historyService = processEngine.getHistoryService();
-        ManagementService managementService = processEngine.getManagementService();
+
+        // 4.流程部署：定义一个流程名称，把bpmn文件和png文件报文到数据库
+        Deployment deployment = repositoryService.createDeployment()
+                .name("出差申请流程")
+                .addClasspathResource("bpmn/evection.bpmn")
+                .addClasspathResource("bpmn/evection.png")
+                .deploy();
+
+        // 5.打印信息
+        System.out.println("流程部署id：" + deployment.getId());
+        System.out.println("流程部署name：" + deployment.getName());
+
     }
+
 
 }
