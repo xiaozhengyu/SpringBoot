b2aec14696c4e75c34b2dd367568f1e69c7b6834
xiaozhengyu <zhengyupython@qq.com>
2020-12-25 17:38:41 +0800

    act

--- activiti/activiti05/src/test/java/com/xzy/activiti/ActivitiApplicationTests.java
+++ activiti/activiti05/src/test/java/com/xzy/activiti/ActivitiApplicationTests.java
@@ -2,6 +2,7 @@ package com.xzy.activiti;
 
 import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,14 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 class ActivitiApplicationTests {
 
     /**
-     * 测试流程部署
+     * 流程部署
      */
     @Test
     void testProcessDeployment() {
 
         // 1.获取ProcessEngineConfiguration
-        //ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("配置文件名");
-        //ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("配置文件名","bean的id");
         ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
 
         // 2.获取ProcessEngine
@@ -41,5 +40,30 @@ class ActivitiApplicationTests {
 
     }
 
+    /**
+     * 创建流程实例（启动流程）
+     */
+    @Test
+    void testProcessInstanceStart() {
+        // Note：创建流程实例前需要先部署流程（具体步骤参照上文的testProcessDeployment()方法）
+
+        // 1.获取ProcessEngine
+        ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+
+        // 2.获取RuntimeService：流程运行管理类
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+
+        // 3.根据定义流程时指定的key，创建流程实例（启动流程）
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myEvection");
+
+        // 4.打印信息
+        System.out.println("流程定义ID："+processInstance.getProcessDefinitionId());
+        System.out.println("流程定义Key："+processInstance.getProcessDefinitionKey());
+        System.out.println("流程定义名称："+processInstance.getProcessDefinitionName());
+        System.out.println("流程定义版本号："+processInstance.getProcessDefinitionVersion());
+        System.out.println("流程实例ID："+processInstance.getProcessInstanceId());
+
+    }
 
 }
